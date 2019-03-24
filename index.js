d0a82d3b332cf3c314cc215a96e67eac6c55bf54
A-Arndt <aarnd649@mtroyal.ca>
2019-03-24 02:11:19 -0600

    Add filtering for specific data

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+d3.csv("city_of_calgary_census_2016.csv").then(function(data) {
+    let initialValue = 0;
 
-
-d3.csv("city_of_calgary_census_2016.csv", function(data) {
-    console.log(data);
+    let totalPop = data.reduce( function (accumulator, currentValue) { 
+        return accumulator + Number(currentValue.RES_CNT);
+    }, initialValue);
+    
+    
+    let communities = ['edgemont', 'acadia', 'banf trail', 'crescent heights', 'panorama hills'];
+    console.log(communities);
+    console.log(communities.includes('acadia'));
+    let filteredData = data.filter(neededCommunity);
 });
+
+
+
+function neededCommunity(obj) {
+    let communities = ['edgemont', 'acadia', 'banff trail', 'crescent heights', 'panorama hills'];
+    return communities.includes(obj.NAME.toLowerCase());
+}
